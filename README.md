3c362035ae8265c719b46833ce76bc152e312914
icoo08217 <icoo08216@gmail.com>
2022-10-11 13:33:37 +0900

    환경세팅

---
+++ README.md
@@ -0,0 +1,1 @@
+# Spring_Record_Pay
---
+++ src/main/java/com/ll/exam/springmelonpay/SpringMelonPayApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.springmelonpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMelonPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMelonPayApplication.class, args);
+    }
+
+}
